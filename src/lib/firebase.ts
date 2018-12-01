15e5a70edd902e3d3425f1e57cdd42ac2899302a
sjsakib <sjsakib.bd@gmail.com>
2018-12-01 22:46:06 +0600

    enabled offline persistence

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,4 +15,9 @@ firebase.initializeApp(config);
 const settings = { timestampsInSnapshots: true };
 firebase.firestore().settings(settings);
 
+firebase.firestore().enablePersistence().catch(reason => {
+  console.log(reason);
+  window.alert('Failed to enable offline persistence');
+});
+
 export default firebase;
